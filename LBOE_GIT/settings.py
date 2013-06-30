b6f4a86d214dae3053c9c32792ad2e2aff400a47
supratim <supratim@supratim-T61.(none)>
2013-07-01 00:22:21 +0530

    Basic login

--- LBOE_GIT/settings.py
+++ LBOE_GIT/settings.py
@@ -71,6 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/code/lboe/static/',
 )
 
 # List of finder classes that know how to find static files in
@@ -110,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/code/lboe/templates/",
 )
 
 INSTALLED_APPS = (
